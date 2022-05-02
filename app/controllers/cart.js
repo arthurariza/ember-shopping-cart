efefbe0fd4cedeadeed1c6261b7f4998d827ee24
arthurariza <tucaariza@gmail.com>
2022-05-02 18:28:06 -0300

    feat: Shopping Cart Service
    >ember g component-class general-container
    >ember g component-class product/details
    >ember g service shopping-cart

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -1,6 +1,9 @@
 import Controller from '@ember/controller';
+import { service } from '@ember/service';
 
 export default class CartController extends Controller {
+  @service('shopping-cart') cart;
+
   get subTotal() {
     return this.model.reduce((acc, { price }) => {
       return acc + price;
